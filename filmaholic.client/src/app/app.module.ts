a60c987afddda9752e39c0e45b9a56610f590be1
JoaoRMInacio <202200663@estudantes.ips.pt>
2026-01-10 16:35:26 +0000

    Formulário do Login feito

--- filmaholic.client/src/app/app.module.ts
+++ filmaholic.client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
